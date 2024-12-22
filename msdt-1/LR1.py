f6b15c7a1ac74a0a818a22e5dbcf1d58960aa37d
Kabaeva Alena <vip.kabaeva2018@mail.ru>
2024-12-22 14:43:07 +0400

    Упрощение метода score и удаление дублирования

--- msdt-1/LR1.py
+++ msdt-1/LR1.py
@@ -15,39 +15,36 @@ class TennisGameDefactored1:
             self.p2points += 1
 
     def score(self):
-        result = ""
-        tempScore = 0
-        if (self.p1points == self.p2points):
-            result = {
-                0: "Love-All",
-                1: "Fifteen-All",
-                2: "Thirty-All",
-                3: "Forty-All",
-            }.get(self.p1points, "Deuce")
-        elif (self.p1points >= 4 or self.p2points >= 4):
-            minusResult = self.p1points - self.p2points
-            if (minusResult == 1):
-                result = "Advantage " + self.player1Name
-            elif (minusResult == -1):
-                result = "Advantage " + self.player2Name
-            elif (minusResult >= 2):
-                result = "Win for " + self.player1Name
-            else:
-                result = "Win for " + self.player2Name
+        if self.p1points == self.p2points:
+            if self.p1points < 4:
+                return {
+                    0: "Love-All",
+                    1: "Fifteen-All",
+                    2: "Thirty-All",
+                    3: "Forty-All",
+                }[self.p1points]
+            return "Deuce"
+
+        if self.p1points >= 4 or self.p2points >= 4:
+            return self.determine_winner()
+
+        return self.get_score()
+
+    def determine_winner(self):
+        minusResult = self.p1points - self.p2points
+        if minusResult == 1:
+            return "Advantage " + self.player1Name
+        elif minusResult == -1:
+            return "Advantage " + self.player2Name
+        elif minusResult >= 2:
+            return "Win for " + self.player1Name
         else:
-            for i in range(1, 3):
-                if (i == 1):
-                    tempScore = self.p1points
-                else:
-                    result += "-"
-                    tempScore = self.p2points
-                result += {
-                    0: "Love",
-                    1: "Fifteen",
-                    2: "Thirty",
-                    3: "Forty",
-                }[tempScore]
-        return result
+            return "Win for " + self.player2Name
+
+    def get_score(self):
+        score_map = ["Love", "Fifteen", "Thirty", "Forty"]
+        return score_map[self.p1points] + "-" + score_map[self.p2points]
+
 
 
 class TennisGameDefactored2:
