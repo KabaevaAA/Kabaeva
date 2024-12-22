a9db4986a97756dbf7fbcb9902c124b8b7910d41
Kabaeva Alena <vip.kabaeva2018@mail.ru>
2024-12-22 14:45:08 +0400

    Приведение класса TennisGameDefactored2 к стандартам PEP8

--- msdt-1/LR1.py
+++ msdt-1/LR1.py
@@ -48,102 +48,56 @@ class TennisGameDefactored1:
 
 
 class TennisGameDefactored2:
+    """Класс для игры в теннис с улучшенной логикой счета."""
+
     def __init__(self, player1Name, player2Name):
+        """Инициализация игры с двумя игроками."""
         self.player1Name = player1Name
         self.player2Name = player2Name
         self.p1points = 0
         self.p2points = 0
 
     def won_point(self, playerName):
+        """Увеличивает очки игрока."""
         if playerName == self.player1Name:
-            self.P1Score()
+            self.p1points += 1
         else:
-            self.P2Score()
+            self.p2points += 1
 
     def score(self):
-        result = ""
-        if (self.p1points == self.p2points and self.p1points < 4):
-            if (self.p1points == 0):
-                result = "Love"
-            if (self.p1points == 1):
-                result = "Fifteen"
-            if (self.p1points == 2):
-                result = "Thirty"
-            if (self.p1points == 3):
-                result = "Forty"
-            result += "-All"
-        if (self.p1points == self.p2points and self.p1points > 3):
-            result = "Deuce"
+        """Возвращает текущий счет игры."""
+        if self.p1points == self.p2points and self.p1points < 4:
+            return self.get_tied_score()
+        elif self.p1points >= 4 or self.p2points >= 4:
+            return self.determine_winner()
 
-        P1res = ""
-        P2res = ""
-        if (self.p1points > 0 and self.p2points == 0):
-            if (self.p1points == 1):
-                P1res = "Fifteen"
-            if (self.p1points == 2):
-                P1res = "Thirty"
-            if (self.p1points == 3):
-                P1res = "Forty"
+        return self.get_score()
 
-            P2res = "Love"
-            result = P1res + "-" + P2res
-        if (self.p2points > 0 and self.p1points == 0):
-            if (self.p2points == 1):
-                P2res = "Fifteen"
-            if (self.p2points == 2):
-                P2res = "Thirty"
-            if (self.p2points == 3):
-                P2res = "Forty"
+    def get_tied_score(self):
+        """Возвращает счет при равенстве очков."""
+        return {
+            0: "Love-All",
+            1: "Fifteen-All",
+            2: "Thirty-All",
+            3: "Forty-All",
+        }[self.p1points]
 
-            P1res = "Love"
-            result = P1res + "-" + P2res
+    def determine_winner(self):
+        """Определяет победителя игры."""
+        minus_result = self.p1points - self.p2points
+        if minus_result == 1:
+            return "Advantage " + self.player1Name
+        elif minus_result == -1:
+            return "Advantage " + self.player2Name
+        elif minus_result >= 2:
+            return "Win for " + self.player1Name
+        else:
+            return "Win for " + self.player2Name
 
-        if (self.p1points > self.p2points and self.p1points < 4):
-            if (self.p1points == 2):
-                P1res = "Thirty"
-            if (self.p1points == 3):
-                P1res = "Forty"
-            if (self.p2points == 1):
-                P2res = "Fifteen"
-            if (self.p2points == 2):
-                P2res = "Thirty"
-            result = P1res + "-" + P2res
-        if (self.p2points > self.p1points and self.p2points < 4):
-            if (self.p2points == 2):
-                P2res = "Thirty"
-            if (self.p2points == 3):
-                P2res = "Forty"
-            if (self.p1points == 1):
-                P1res = "Fifteen"
-            if (self.p1points == 2):
-                P1res = "Thirty"
-            result = P1res + "-" + P2res
-
-        if (self.p1points > self.p2points and self.p2points >= 3):
-            result = "Advantage " + self.player1Name
-
-        if (self.p2points > self.p1points and self.p1points >= 3):
-            result = "Advantage " + self.player2Name
-
-        if (self.p1points >= 4 and self.p2points >= 0 and (self.p1points - self.p2points) >= 2):
-            result = "Win for " + self.player1Name
-        if (self.p2points >= 4 and self.p1points >= 0 and (self.p2points - self.p1points) >= 2):
-            result = "Win for " + self.player2Name
-        return result
-
-    def SetP1Score(self, number):
-        for i in range(number):
-            self.P1Score()
-
-    def SetP2Score(self, number):
-        for i in range(number):
-            self.P2Score()
-
-    def P1Score(self):
-        self.p1points += 1
-
-    def P2Score(self):
-        self.p2points += 1
+    def get_score(self):
+        """Возвращает строку с текущим счетом."""
+        score_map = ["Love", "Fifteen", "Thirty", "Forty"]
+        return score_map[self.p1points] + "-" + score_map[self.p2points]
 
 
 class TennisGameDefactored3:
